9f88bd7a76332c308929f1d1388baa4917ed5341
hongboqu <2267606106@qq.com>
2025-03-21 19:38:54 +0800

    实现了比例尺，显示大小，经纬度

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useEffect, useState } from "react";
+import leaflet from "leaflet";
 
-function App() {
-  const [count, setCount] = useState(0)
-  const name = import.meta.env.VITE_APP_NAME;
+export default function App() {
+  //
+  const [clickPosition, setClickPosition] = useState<{ lat: number, lng: number } | null>(null);
+
+  const mapRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (mapRef.current) {
+      const map = leaflet.map(mapRef.current, {
+        zoomControl: false,
+        attributionControl: false,
+      }).setView([35.771944, 120.024444], 7);
+
+      //经纬度
+      map.on("click", (e) => {
+        setClickPosition({
+          lat: e.latlng.lat,
+          lng: e.latlng.lng,
+        });
+      });
+
+      leaflet.control.scale({
+        imperial: false,
+        position: "bottomright",
+      }).addTo(map);
+      leaflet.control.zoom({ position: "bottomright" }).addTo(map);
+      leaflet
+        .tileLayer(
+          "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+        ).addTo(map);
+      leaflet
+        .marker([35.771944, 120.024444])
+        .addTo(map)
+        .openPopup();
+    }
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>{name}</p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div ref={mapRef} style={{ width: "100vw", height: "100vh" }}></div>
+      {clickPosition &&
+        (<div
+          style={{
+            position: "absolute",
+            bottom: "0px",
+            right: "5px",
+            color:"#fff",
+            zIndex: 999,
+          }}>
+          <div>经度：{clickPosition.lat.toFixed(2)}</div>
+          <div>纬度：{clickPosition.lng.toFixed(2)}</div>
+        </div>)
+      }
+      <div
+        style={{
+          position: "absolute",
+          bottom: "0px",
+          right: "130px",
+          color:"#fff",
+          zIndex: 999,
+        }}>
+          <div>姓名: 曲泓勃</div>
+          <div>学号: 23020007099</div>
+     </div>
     </>
-  )
+  );
 }
-
-export default App
